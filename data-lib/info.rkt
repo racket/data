96fb8a44c7d4c82bf758413a81f52565c1fc9fa7
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-31 15:43:46 -0500

    Update base dependency version.

--- data-lib/info.rkt
+++ data-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base"))
+(define deps '(("base" #:version "6.2.900.6")))
 (define build-deps '("rackunit-lib"))
 
 (define pkg-desc "implementation (no documentation) part of \"data\"")
