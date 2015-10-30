6ab83bbd4446ae8b9dbe1a642c1d1429a06a80d8
Robby Findler <robby@racket-lang.org>
2015-10-30 14:55:28 -0500

    improve test suites a little bit to make it easier to track down bugs

--- data-test/tests/data/enumerate/util.rkt
+++ data-test/tests/data/enumerate/util.rkt
@@ -21,39 +21,47 @@
   (syntax-case stx ()
     [(_ e)
      (with-syntax ([line (syntax-line stx)])
-       #'(check-contract/proc line e 'e))]))
+       (syntax/loc stx (check-contract/proc line e 'e)))]))
 
 (define (check-contract/proc line enum enum-code)
   (for ([x (in-range (if (finite-enum? enum)
                          (min (enum-count enum) 100)
                          100))])
-    (contract-exercise
-     (contract (enum-contract enum)
-               (from-nat enum x)
-               'ignore-me
-               'whatever
-               (format "~s, index ~a" enum-code x)
-               #f)))
+    (with-handlers ([exn:fail?
+                     (λ (exn)
+                       (printf "exn exercising when x=~a\n" x)
+                       (raise exn))])
+      (contract-exercise
+       (contract (enum-contract enum)
+                 (from-nat enum x)
+                 'ignore-me
+                 'whatever
+                 (format "~s, index ~a" enum-code x)
+                 #f))))
   
   (when (two-way-enum? enum)
     (let/ec give-up-completely
       (for ([x (in-range 100)])
-        (let/ec give-up-this-attempt
-          (define value
-            (contract-random-generate
-             (enum-contract enum)
-             2
-             (λ (no-generator?)
-               (cond
-                 [no-generator?
-                  (eprintf "no generator for:\n  ~s\n  ~s\n" 
-                           enum-code
-                           (enum-contract enum))
-                  (give-up-completely (void))]
-                 [else
-                  (give-up-this-attempt)]))))
-          (with-handlers ([exn:fail?
-                           (λ (x)
-                             (eprintf "test/data/enumerate: trying ~s\n" value)
-                             (raise x))])
-            (to-nat enum value)))))))
+        (with-handlers ([exn:fail?
+                         (λ (exn)
+                           (printf "exn generating when x=~a\n" x)
+                           (raise exn))])
+          (let/ec give-up-this-attempt
+            (define value
+              (contract-random-generate
+               (enum-contract enum)
+               2
+               (λ (no-generator?)
+                 (cond
+                   [no-generator?
+                    (eprintf "no generator for:\n  ~s\n  ~s\n"
+                             enum-code
+                             (enum-contract enum))
+                    (give-up-completely (void))]
+                   [else
+                    (give-up-this-attempt)]))))
+            (with-handlers ([exn:fail?
+                             (λ (x)
+                               (eprintf "test/data/enumerate: trying ~s\n" value)
+                               (raise x))])
+              (to-nat enum value))))))))
