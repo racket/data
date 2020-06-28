a9e7b138b5fd55f1d90368ee577a7f4ef6fa8a0e
Metaxal <laurent.orseau@gmail.com>
2020-06-28 12:35:18 +0100

    add #:set-node keyword

--- data-test/tests/stress-test-compare-iheaps.rkt
+++ data-test/tests/stress-test-compare-iheaps.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (require data/iheap
-         data/iheap2)
+         data/iheap2
+         racket/unsafe/ops)
 
 ;;;; Using two priority queues with indexed heaps.
 
@@ -12,20 +13,24 @@
   (begin
     (collect-garbage)
     (collect-garbage)
+    (collect-garbage)
     (define pre (current-memory-use))
     (time body ...)
+    #;(collect-garbage) ; don't do that, it may collect things that were created before
+    #;(collect-garbage)
+    #;(collect-garbage)
     (define post (current-memory-use))
     (printf "memory use: ~a\n" (- post pre))))
 
 
 (for ([N (in-list '(1000 100000 1000000 10000000))])
-  (printf "\nN=~a\n" N)
+  (printf "\n\nN=~a\n" N)
   (define lst (build-list N (λ (i) (random (expt 2 31)))))
   ;;; Indexed heaps with internal nodes.
   ;;; The user must keep the *nodes* returned by iheap-add-all!
   ;;; in the same structure containing the user's values.
   ;;; Nodes are opaque.
-  (displayln "* iheap (opaque)")
+  (displayln "\n* iheap (opaque)")
   (define lres1
     (let ()
       (struct element (val [h1-node #:mutable] [h2-node #:mutable])
@@ -38,12 +43,8 @@
       ;; Add the nodes to both heaps and track them in `element`.
       (displayln "*** Add elements")
       (time+memory
-       (define h1elts (iheap-add-all! h1 elts))
-       (define h2elts (iheap-add-all! h2 elts))
-       (for ([h-elt (in-list h1elts)])
-         (set-element-h1-node! (iheap-node-key h-elt) h-elt))
-       (for ([h-elt (in-list h2elts)])
-         (set-element-h2-node! (iheap-node-key h-elt) h-elt)))
+       (iheap-add-all! h1 elts #:set-node! set-element-h1-node!)
+       (iheap-add-all! h2 elts #:set-node! set-element-h2-node!))
 
       ; Remove min for h1, and remove same element in h2
       (displayln "*** Remove elements")
@@ -57,16 +58,24 @@
 
   ;;; Indexed heaps with getter and setter so the user keeps track of the indices.
   ;;; Indices are not opaque.
-  (displayln "* iheap2 (visible indices)")
+  (displayln "\n* iheap2 (visible indices)")
   (define lres2
     (let ()
       (struct element (val [h1-idx #:mutable] [h2-idx #:mutable])
         #:transparent)
+      (define (idx1-set! elt idx)
+        (unsafe-struct*-set! elt 1 idx))
+      (define (idx2-set! elt idx)
+        (unsafe-struct*-set! elt 2 idx))
       (define h1 (make-iheap2 (lambda (e1 e2) (< (element-val e1) (element-val e2)))
-                              #:set-index! set-element-h1-idx!
+                              #:set-index!
+                              #;idx1-set! ; makes the GC blow up??
+                              set-element-h1-idx!
                               #:get-index element-h1-idx))
       (define h2 (make-iheap2 (lambda (e1 e2) (> (element-val e1) (element-val e2)))
-                              #:set-index! set-element-h2-idx!
+                              #:set-index!
+                              #;idx2-set!
+                              set-element-h2-idx!
                               #:get-index element-h2-idx))
 
       (define elts (map (λ (x) (element x #f #f)) lst))
