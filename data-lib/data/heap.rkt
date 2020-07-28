14effec9f652bec4e337eab02d429426cc5defb9
Metaxal <laurent.orseau@gmail.com>
2020-07-28 13:24:38 +0100

    reorder exports as before

--- data-lib/data/heap.rkt
+++ data-lib/data/heap.rkt
@@ -313,6 +313,33 @@
 
 ;; --------
 
+(provide
+ (contract-out
+  #:unprotected-submodule unchecked
+  [make-heap        (->  (and (procedure-arity-includes/c 2)
+                              (unconstrained-domain-> any/c))
+                         heap?)]
+  [heap?            (->  any/c boolean?)]
+  [heap-count       (->  heap? exact-nonnegative-integer?)]
+  [heap-add!        (->* (heap?) () #:rest list? void?)]
+  [heap-add-all!    (->  heap? (or/c list? vector? heap?) void?)]
+  [heap-min         (->  heap? any/c)]
+  [heap-remove-min! (->  heap? void?)]
+  [heap-remove!     (->* (heap? any/c) [#:same? (-> any/c any/c any/c)] boolean?)]
+  [heap-remove-eq!  (->  heap? any/c boolean?)]
+
+  [vector->heap     (-> (and (procedure-arity-includes/c 2)
+                             (unconstrained-domain-> any/c))
+                        vector?
+                        heap?)]
+  [heap->vector     (-> heap? vector?)]
+  [heap-copy        (-> heap? heap?)]
+
+  [in-heap          (-> heap? sequence?)]
+  [in-heap/consume! (-> heap? sequence?)]))
+
+(provide heap-sort!) ; checks done in-function
+
 (module+ test-util
   (provide valid-heap?
            fittest-block-size
@@ -329,31 +356,3 @@
                  (loop (vt-leftchild i) this)
                  (loop (vt-rightchild i) this))]
            [else #t]))])))
-
-
-(provide
- (contract-out
- #:unprotected-submodule unchecked
- [make-heap        (->  (and (procedure-arity-includes/c 2)
-                             (unconstrained-domain-> any/c))
-                        heap?)]
- [heap?            (->  any/c boolean?)]
- [heap-count       (->  heap? exact-nonnegative-integer?)]
- [heap-add!        (->* (heap?) () #:rest list? void?)]
- [heap-add-all!    (->  heap? (or/c list? vector? heap?) void?)]
- [heap-min         (->  heap? any/c)]
- [heap-remove-min! (->  heap? void?)]
- [heap-remove!     (->* (heap? any/c) [#:same? (-> any/c any/c any/c)] boolean?)]
- [heap-remove-eq!  (->  heap? any/c boolean?)]
-
- [vector->heap (-> (and (procedure-arity-includes/c 2)
-                             (unconstrained-domain-> any/c))
-                   vector?
-                   heap?)]
- [heap->vector (-> heap? vector?)]
- [heap-copy    (-> heap? heap?)]
-
- [in-heap          (-> heap? sequence?)]
- [in-heap/consume! (-> heap? sequence?)]))
-
-(provide heap-sort!) ; checks done in-function
